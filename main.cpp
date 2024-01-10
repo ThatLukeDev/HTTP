dc45717ff75f72e3dda626cfa7dabc313ea5be41
ThatLukeDev <luke@l2j.co.uk>
2024-01-10 19:42:50 +0000

    added responses and http query parser, error with repeat access (works only once)

--- main.cpp
+++ main.cpp
@@ -3,12 +3,7 @@
 #include "transfer.hpp"
 
 int main() {
-	transfer.bindPort(1234, [&](transfer::packet input) {
-		std::cout << "Message: " << (char*)input.data;
-		std::cout << "Response: ";
-		const char* response = "this is the full response";
-		return transfer::packet((void*)response, strlen(response));
-	});
+	HTTP::server(8080).start();
 
 	return 0;
 }
--- transfer.hpp
+++ transfer.hpp
@@ -1,8 +1,8 @@
 #ifndef TRANSFER_HPP
 #define TRANSFER_HPP
 
-#include <cstring>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <functional>
 
@@ -10,6 +10,29 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+
+#include <iostream>
+#include <fstream>
+
+namespace file {
+	struct packet {
+		void* data;
+		int size;
+
+		packet(void* _data, int _size) : data(_data), size(_size) { }
+	};
+
+	file::packet read(char* _filename) {
+		std::ifstream sr;
+		sr.open(_filename);
+		sr.seekg(0, std::ios::end);
+		file::packet _out = file::packet(malloc(sr.tellg()), sr.tellg());
+		sr.seekg(0);
+		sr.read((char*)_out.data, _out.size);
+		sr.close();
+		return _out;
+	}
+}
 
 static class transfer {
 
@@ -20,13 +43,6 @@ static class transfer {
 	std::vector<int> ports = std::vector<int>();
 
 public:
-
-	struct packet {
-		void* data;
-		int size;
-
-		packet(void* _data, int _size) : data(_data), size(_size) { }
-	};
 
 	int openSocket(unsigned int _port) {
 		int newSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -62,8 +78,8 @@ public:
 		}
 	}
 
-	int bindSocket(unsigned int _socket, std::function<packet(packet)> func) {
-		while (std::find(sockets.begin(), sockets.end(), _socket) != sockets.end()) {
+	int bindSocket(unsigned int _socket, std::function<file::packet(file::packet)> func) {
+		while (1|| std::find(sockets.begin(), sockets.end(), _socket) != sockets.end()) {
 			listen(_socket, MAX_AWAITS);
 			sockaddr_in address = sockaddr_in();
 			socklen_t addresslength = sizeof(address);
@@ -84,13 +100,19 @@ public:
 				return -1;
 			}
 
-			packet response = func(packet(&buffer, strlen((char*)&buffer)));
-
-			if (response.size == 0) {
-				continue;
+			int strsize = BUFFER_SIZE - 1;
+			for (int i = 0; i < BUFFER_SIZE - 1; i++) {
+				if (buffer[i] == 0) {
+					strsize = i;
+				}
 			}
-			if (response.size == -1) {
+			file::packet response = func(file::packet(&buffer, strsize));
+
+			if (response.size == -256) {
 				break;
+			}
+			if (response.size <= 0) {
+				continue;
 			}
 
 			if (write(connection, response.data, response.size) < 0) {
@@ -100,7 +122,6 @@ public:
 
 			close(connection);
 		}
-		closeSocket(_socket);
 
 		return 0;
 	}
@@ -113,7 +134,7 @@ public:
 		}
 	}
 
-	int bindPort(unsigned int _port, std::function<packet(packet)> func) {
+	int bindPort(unsigned int _port, std::function<file::packet(file::packet)> func) {
 		int _socket = -1;
 
 		for (int i = 0; i < ports.size(); i++) {
@@ -132,5 +153,123 @@ public:
 	}
 
 } transfer;
+
+namespace HTTP {
+
+	class message {
+	public:
+		char* protocol;
+		char* path;
+		char* headers;
+		char* body;
+		int bodyLen;
+
+		message() { }
+		message(char* __msg, int _size) {
+			char* _msg = (char*)malloc(strlen(__msg));
+			memcpy(_msg, __msg, strlen(__msg));
+			char* msgS[4];
+			int v = 0;
+			int b = -1;
+			msgS[0] = _msg;
+			for (int i = 0; i < _size; i++) {
+				if (v < 2) {
+					if (_msg[i] == ' ') {
+						v++;
+						msgS[v] = (char*)((unsigned long)_msg + (unsigned long)(i + 1));
+						*(msgS[v] - 1) = 0;
+					}
+				}
+				else {
+					if (_msg[i] == '\n') {
+						b++;
+						if (b == 0) {
+							v++;
+							if (v > 3) continue;
+							msgS[v] = _msg + ((i + 1) * sizeof(char));
+							*(msgS[v] - 1) = 0;
+						}
+						if (b == 2) {
+							v++;
+							if (v > 3) continue;
+							msgS[v] = _msg + ((i + 1) * sizeof(char));
+							*(msgS[v] - 2) = 0;
+						}
+					}
+					else {
+						b = 0;
+					}
+				}
+			}
+			protocol = msgS[0];
+			path = msgS[1];
+			headers = msgS[2];
+			body = msgS[3];
+			bodyLen = _size - (int)(body - protocol);
+		}
+
+		char* absolutePath() {
+			bool _def = false;
+			char* _out = (char*)malloc(strlen(path));
+			memcpy(_out, path, strlen(path));
+
+			if (_out[0] != '/') {
+				_def = true;
+			}
+			for (int i = 0; i < strlen(_out); i++) {
+				if (_out[i] == '.' && _out[i + 1] == '.' && _out[i + 2] == '/') {
+					_def = true;
+				}
+			}
+
+			if (_def) {
+				_out[0] = '/';
+				_out[1] = 0;
+				return _out;
+			}
+
+			return _out;
+		}
+	};
+
+	class server {
+	public:
+		char* HOME = (char*)"./index.html";
+
+		int port = 80;
+		bool running = false;
+
+		server() { }
+		server(int _port) : port(_port) { }
+		~server() { if (running) stop(); }
+
+		void start() {
+			running = true;
+			transfer.bindPort(port, [&](file::packet _data) {
+				HTTP::message msg = HTTP::message((char*)_data.data, _data.size);
+
+				if (strcmp(msg.protocol, "GET") == 0) {
+					char* absPath = msg.absolutePath();
+					char* path;
+					if (strcmp(absPath, "/") == 0) {
+						path = HOME;
+					}
+					else {
+						path = (char*)malloc(strlen(absPath) + 1);
+						path[0] = '.';
+						memcpy(path + 1, absPath, strlen(absPath));
+					}
+
+					return file::read(path);
+				}
+
+				return _data;
+			});
+		}
+		void stop() {
+			transfer.closePort(port);
+		}
+	};
+}
 
 #endif
